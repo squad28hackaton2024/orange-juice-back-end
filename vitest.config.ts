469ad0b7ff39dcf01864abc5d8bc55445399a030
lucasnather <natherlucas@gmail.com>
2024-01-24 14:59:54 -0400

    feat: configurando ambiente para os testes de ponta a ponto

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,10 @@ import tsConfigPaths from 'vitest-tsconfig-paths'
 
 export default defineConfig({
     test: {
-        globals: true
+        globals: true,
+        environmentMatchGlobs: [
+			['src/controller/**', 'prisma']
+		]
     },
     plugins: [tsConfigPaths()]
 })
