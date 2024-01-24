00e7bbb668bdec8e793f70c6f289299dcc201539
lucasnather <natherlucas@gmail.com>
2024-01-24 14:46:02 -0400

    feat: criação do serviço de autenticação so usuário

---
+++ src/services/usuarios/authenticate-usuarios-service.ts
@@ -0,0 +1,34 @@
+import { CredenciaisInvalidasError } from "@/error/credenciais-invalidas-error";
+import { IUsuarios } from "@/interface/i-usuarios";
+import { PasswordHash } from "@/uteis/password-hash";
+import { Usuarios } from "@prisma/client";
+
+interface AuthenticateUsuariosRequest {
+    email: string
+    senha: string
+}
+
+interface AuthenticateUsuariosResponse {
+    usuarios: Usuarios
+}
+
+export class AuthenticateUsuariosService {
+
+    constructor(private usuariosRepository: IUsuarios) {}
+
+    async handle(data: AuthenticateUsuariosRequest): Promise<AuthenticateUsuariosResponse> {
+        const usuarios = await this.usuariosRepository.findByEmail(data.email)
+
+        if(!usuarios) throw new CredenciaisInvalidasError()
+
+        const passwordHash = new PasswordHash()
+
+        const aSenhaEValida = await passwordHash.compareSenha(data.senha, usuarios.senha)
+
+        if(!usuarios) throw new CredenciaisInvalidasError()
+
+        return {
+            usuarios
+        }
+    }
+}
