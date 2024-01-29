3a0e1409ae27c8b93cc19dbdb2df97e01dfe7a2a
Gabriel <gabriel.mazer@outlook.com>
2024-01-29 19:42:30 -0300

    feat: teste ponta-a-ponta controller encontrar projeto usuário id

---
+++ src/controller/projetos/find-by-usuario-id.spec.ts
@@ -0,0 +1,60 @@
+import { app } from '@/app'
+import request from 'supertest'
+import path from 'node:path'
+
+const filePath = path.resolve(__dirname, '../../../tmp/storage', 'chipi.jpeg')
+
+describe('Find By Usuários Id Projetos [GET]', () => {
+
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('Deve encontrar todos os projetos pelo id de usuario', async () => {
+        await request(app.server)
+                .post('/usuarios')
+                .send({
+                    nome: 'lucas',
+                    sobrenome: 'nather',
+                    email: 'lucas@email.com',
+                    senha: 'funciona por favor'
+                }) 
+
+        const usuario = await request(app.server)
+                            .post('/usuarios/login')
+                            .send({
+                                email: 'lucas@email.com',
+                                senha: 'funciona por favor'
+                            })
+
+        const { token } = usuario.body
+
+        await request(app.server)
+                .post('/projetos')
+                .set('Authorization', `Bearer ${token}`)
+                .field('titulo', 'projeto top')
+                .field('descricao', 'que descricao boa')
+                .field('link', 'aaaaa')
+                .field('tags', 'ux , dev')
+                .attach('imagens', filePath)
+
+        await request(app.server)
+                .post('/projetos')
+                .set('Authorization', `Bearer ${token}`)
+                .field('titulo', 'projeto legal')
+                .field('descricao', 'que descricao bacana')
+                .field('link', 'pega ai por favor')
+                .field('tags', 'front , back')
+                .attach('imagens', filePath)
+
+        await request(app.server)
+                .get('/projetos/usuarios')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200)
+
+    })
+})
